d8c754c040f9d8b59c561c74e037e6d5327c3da6
rachitpoudel101 <rachit300poudel@gmail.com>
2023-12-18 20:14:53 +0545

    simple change  in UI

--- app.py
+++ app.py
@@ -14,6 +14,21 @@ class ImageToPDFConverter:
         title_label = tk.Label(self.root, text="Image to PDF Converter", font=("Helvetica", 16, "bold"))
         title_label.pack(pady=20)
 
+        select_images_button = tk.Button(self.root, text="Select Images", command=self.select_images)
+        select_images_button.pack(pady=(0,10))
+
+        self.selected_images_listbox.pack(pady=(0,10), fill=tk.BOTH, expand= True)
+        label =tk.Label(self.root,text="Enter the outpur PDF name")
+        label.pack()
+
+        pdf_name_entry =tk.Entry(self.root, textlabel = self.output_pdf_name,width =40, justify="center")
+        pdf_name_entry.pack()
+        
+        convert_button = tk.Button(self.root, text="Convert to PDF", command=self.convert_images_to_pdf)
+        convert_button.pack(pady=(20, 40))
+        
+
+
 def main():
     root = tk.Tk()  # Corrected the case of 'Tk'
     root.title("Image to PDF")
