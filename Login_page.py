e7b80471ffbc4d84e796933adabbbdf85647e074
rachitpoudel101 <rachit300poudel@gmail.com>
2023-12-28 21:36:02 +0545

    Just the topic is created

--- Login_page.py
+++ Login_page.py
@@ -2,9 +2,12 @@ from tkinter import*
 from PIL import ImageTk
 
 root =Tk()
-
+root .geometry('990x660+50+50')
 
 bgImage=ImageTk.PhotoImage(file='bg.png')
 bglabel=Label(root,image=bgImage)
-bglabel.grid(row=0,column=0)
+bglabel.place(x=0,y=0)
+
+headng = Label(root,text='User Login')
+headng.place(x=605,y=120)
 root.mainloop()
