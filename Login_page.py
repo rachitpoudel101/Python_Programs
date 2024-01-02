5b1c433d03b03dee0117e4d4bc197ca2612a9260
rachitpoudel101 <rachit300poudel@gmail.com>
2024-01-02 20:16:37 +0545

    password entry is done

--- Login_page.py
+++ Login_page.py
@@ -1,9 +1,13 @@
 from tkinter import*
 from PIL import ImageTk
 #Functionality part
-def on_enter(event):
+def user_enter(event):
     if Username.get()=='Username':
         Username.delete(0,END)
+
+def password_enter(event):
+    if Password.get()=='Password':
+        Password.delete(0,END)
         
 #GUI part 
 
@@ -23,9 +27,20 @@ Username.place(x=590,y=200)
 Username.insert(0,'Username')
 
 
-Username.bind('<FocusIn>',on_enter)
+Username.bind('<FocusIn>',user_enter)
 
 
-Frame(Login_Window,width=250,height=2,bd=0,fg='firebrick1').place(x=590, y=225)
+Frame1=Frame(Login_Window,width=250,height=2,bg='firebrick1')
+Frame1.place(x=590, y=225)
+
+Password=Entry(Login_Window,width=25,font=('Microsoft Yahei UI Light',11,'bold'),bd=0,fg='firebrick1')
+Password.place(x=590,y=260)
+Password.insert(0,'Password')
+
+
+Password.bind('<FocusIn>',password_enter)
+
+Frame2=Frame(Login_Window,width=250,height=2,bg='firebrick1')
+Frame2.place(x=590, y=282)
 
 Login_Window.mainloop()
