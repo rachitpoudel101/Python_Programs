a3b98b49217bb3084bd6f9585e88549583ed4864
rachitpoudel101 <rachit300poudel@gmail.com>
2024-01-14 19:54:55 +0545

    ADD SOME FUNCTIALITY IN THE PASSWORD

--- Login_page.py
+++ Login_page.py
@@ -9,6 +9,16 @@ def password_enter(event):
     if Password.get()=='Password':
         Password.delete(0,END)
         
+def hide():
+    openEye.config(file= "closeye.png")
+    Password.config(show='*')
+    eyeButton.config(command=show)
+
+def show():
+    openEye.config(file="openeye.png")
+    Password.config(show='')
+    eyeButton.config(command=hide)
+        
 #GUI part 
 
 Login_Window =Tk()
@@ -43,8 +53,12 @@ Password.bind('<FocusIn>',password_enter)
 Frame2=Frame(Login_Window,width=250,height=2,bg='firebrick1')
 Frame2.place(x=590, y=282)
 
-closeEye=PhotoImage(file='closeye.png')
-eyeButton = Button(Login_Window,image=closeEye)
+openEye=PhotoImage(file='openeye.png')
+eyeButton = Button(Login_Window,image=openEye,bd =0,bg="white",activebackground="white",cursor="hand2",command=hide)
 eyeButton.place(x=800,y=248)
+
+Forget_Button = Button(Login_Window,text="Forgot Password",bd =0,bg="white",activebackground="white",cursor="hand2",font=('Microsoft Yahei UI Light',9,'bold'),fg='firebrick1')
+Forget_Button.place(x=715,y=295)
+
 
 Login_Window.mainloop()
