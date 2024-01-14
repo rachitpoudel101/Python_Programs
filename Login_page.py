cf724458e695179b62bfa336f9246f1ed6bd501e
rachitpoudel101 <rachit300poudel@gmail.com>
2024-01-15 00:13:24 +0545

    Add login button nad some functionality

--- Login_page.py
+++ Login_page.py
@@ -60,5 +60,11 @@ eyeButton.place(x=800,y=248)
 Forget_Button = Button(Login_Window,text="Forgot Password",bd =0,bg="white",activebackground="white",cursor="hand2",font=('Microsoft Yahei UI Light',9,'bold'),fg='firebrick1')
 Forget_Button.place(x=715,y=295)
 
+loginButton =Button(Login_Window, text="Login", font=('open Sans',16,'bold'),fg='white',bg='firebrick1',activeforeground="white",activebackground="firebrick1",cursor='hand2',bd=0,width=19)
+loginButton.place(x=578,y=350)
+
+
+# orLabel =Label(Login_Window,text="-------------------OR------------------",font=('Open Sans',16),fg='firebrick1')
+# orLabel
 
 Login_Window.mainloop()
