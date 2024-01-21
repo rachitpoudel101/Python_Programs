b9924950b1754e589f154c741a22536062567614
rachitpoudel101 <rachit300poudel@gmail.com>
2024-01-21 22:46:56 +0545

    Perfect square from user input

--- perfect_number.py
+++ perfect_number.py
@@ -1,11 +1,14 @@
-n = 28
+# Get user input for the number
+n = int(input("Enter a number: "))
 sum = 0
 
+# Calculate the sum of divisors
 for i in range(1, n):
     if n % i == 0:
         sum = sum + i
 
+# Check if the number is a perfect number
 if sum == n:
-    print("The number is a Perfect number")
+    print(f"{n} is a Perfect number")
 else:
-    print("The number is not a Perfect number")
+    print(f"{n} is not a Perfect number")
