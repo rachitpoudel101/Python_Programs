9d1a4992735da98440edee029ab0a56dce59f0ab
rachitpoudel101 <rachit300poudel@gmail.com>
2024-01-23 21:27:00 +0545

    Add the email feild in the sign up page

--- signup.py
+++ signup.py
@@ -18,5 +18,9 @@ frame.place(x=554 ,y= 100)
 headng = Label(frame,text='CREATE AN ACCOUNT',font=('Microsoft Yahei UI Light',18,'bold'),bg='white',fg='firebrick1')
 headng.grid(row=0 ,column=0,padx=10,pady=10)
 
+emaillabel =Label(frame,text='Email',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+emaillabel.grid(row=1,column=0, sticky='w',padx=25)
+emailentry =Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'))
+emailentry.grid(row=2,column=0,sticky='w',padx=25)
 
 Signup_Window.mainloop()
