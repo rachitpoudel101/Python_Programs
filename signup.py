207a01e980f85852b31b29b3b9ac9eba9aa81352
rachitpoudel101 <rachit300poudel@gmail.com>
2024-01-24 22:56:11 +0545

    Passowrd and username text feild is added

--- signup.py
+++ signup.py
@@ -19,8 +19,24 @@ headng = Label(frame,text='CREATE AN ACCOUNT',font=('Microsoft Yahei UI Light',1
 headng.grid(row=0 ,column=0,padx=10,pady=10)
 
 emaillabel =Label(frame,text='Email',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
-emaillabel.grid(row=1,column=0, sticky='w',padx=25)
-emailentry =Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'))
+emaillabel.grid(row=1,column=0, sticky='w',padx=25,pady=(10,0))
+emailentry =Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
 emailentry.grid(row=2,column=0,sticky='w',padx=25)
+
+usernamelabel =Label(frame,text='Username',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+usernamelabel.grid(row=3,column=0, sticky='w',padx=25,pady=(10,0))
+usernameentry =Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
+usernameentry.grid(row=4,column=0,sticky='w',padx=25)
+
+Passwordlabel =Label(frame,text='Password',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+Passwordlabel.grid(row=5,column=0, sticky='w',padx=25,pady=(10,0))
+Passwordentry =Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
+Passwordentry.grid(row=6,column=0,sticky='w',padx=25)
+
+Conpasswordlabel =Label(frame,text='Confirm Password',font=('Microsoft Yahei UI Light',10,'bold'),bg='white',fg='firebrick1')
+Conpasswordlabel.grid(row=7,column=0, sticky='w',padx=25,pady=(10,0))
+Conpasswordentry =Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),fg='white',bg='firebrick1')
+Conpasswordentry.grid(row=8,column=0,sticky='w',padx=25)
+
 
 Signup_Window.mainloop()
